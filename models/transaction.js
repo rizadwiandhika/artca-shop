5d0030515770e95c5a42dec0e385deb7f7d6a9ff
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-04 23:05:42 +0700

    add: status for trasaction
    status means for pending, paid, or failed

--- models/transaction.js
+++ models/transaction.js
@@ -5,7 +5,8 @@ const Transaction = sequelize.define('transaction', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   type: { type: DataTypes.STRING, allowNull: false },
   totalPrice: { type: DataTypes.DOUBLE, allowNull: false, defaultValue: 0 },
-  address: { type: DataTypes.TEXT }
+  address: { type: DataTypes.TEXT },
+  status: { type: DataTypes.STRING, allowNull: false, defaultValue: 'pending' }
 })
 
 module.exports = Transaction
