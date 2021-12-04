654877fb42b184640265e9c3f4f1ec0bcd11ea39
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-04 20:50:37 +0700

    refactor: remove comment

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get('/product/:id', shopControllers.getArtDetail)
 router.get('/checkout/:artId', shouldLogin(true), shopControllers.getCheckout)
-// router.get('/checkout/:artId', shopControllers.getCheckout)
+
 router.post('/checkout', shouldLogin(true), shopControllers.postCheckout)
 router.get('/home', shopControllers.getIndex)
 
