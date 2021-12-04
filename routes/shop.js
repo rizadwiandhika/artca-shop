88e3fbbaf802b4b3c57c23db5a09e36f7b3a55a0
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-04 19:09:02 +0700

    update: detail product route

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const { shouldLogin } = require('../middleware/auth')
 
 const router = express.Router()
 
-router.get('/art/:id', shopControllers.getArtDetail)
+router.get('/product/:id', shopControllers.getArtDetail)
 router.get('/checkout/:artId', shouldLogin(true), shopControllers.getCheckout)
 // router.get('/checkout/:artId', shopControllers.getCheckout)
 router.post('/checkout', shouldLogin(true), shopControllers.postCheckout)
