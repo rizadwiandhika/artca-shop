6ebceebc4ee597b544657820ec63fa0824234f9a
rizadwiandhika <rizadwiandhika@gmail.com>
2021-12-04 22:58:25 +0700

    style(minor): line adjustment

--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,8 @@ const router = express.Router()
 
 router.get('/product/:id', shopControllers.getArtDetail)
 router.get('/checkout/:artId', shouldLogin(true), shopControllers.getCheckout)
+router.get('/home', shopControllers.getIndex)
 
 router.post('/checkout', shouldLogin(true), shopControllers.postCheckout)
-router.get('/home', shopControllers.getIndex)
 
 module.exports = router
